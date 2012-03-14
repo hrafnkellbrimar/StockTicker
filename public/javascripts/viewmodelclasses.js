0269c173c60507beb9d2ef617c2117ca43c21568
Hrafnkell Brimar Hallmundsson <hrafnkell@hrafnkell-laptop.(none)>
2012-03-14 17:45:25 +0000

    fixed all

--- public/javascripts/viewmodelclasses.js
+++ public/javascripts/viewmodelclasses.js
@@ -1,17 +1,96 @@
 (function() {
+  var __hasProp = Object.prototype.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
 
   $(document).ready(function() {
-    var drugViewModel;
-    drugViewModel = (function() {
+    now.name = prompt("What's your name?", "");
+    drugViewModel(function() {
+      var Cocaine, Crack, Drug, Lsd, coke, crack, lsd;
+      Drug = (function() {
 
-      function drugViewModel() {}
+        function Drug(name) {
+          this.name = name;
+        }
 
-      drugViewModel.nafn = now.name;
+        Drug.prototype.price = function(dollars) {
+          return alert(this.name + (" costs " + dollars));
+        };
 
-      drugViewModel.drugTypes = ko.observableArray(["hass", "LSD"]);
+        Drug.prototype.status = function(status) {
+          return alert(this.name + (" has status " + status));
+        };
 
+        return Drug;
 
-    })();
+      })();
+      Cocaine = (function(_super) {
+
+        __extends(Cocaine, _super);
+
+        function Cocaine() {
+          Cocaine.__super__.constructor.apply(this, arguments);
+        }
+
+        Cocaine.prototype.price = function() {
+          alert("Cocaine price");
+          return Cocaine.__super__.price.call(this, 5);
+        };
+
+        Cocaine.prototype.status = function() {
+          alert("Cocaine status");
+          return Cocaine.__super__.status.call(this, "up");
+        };
+
+        return Cocaine;
+
+      })(Drug);
+      Lsd = (function(_super) {
+
+        __extends(Lsd, _super);
+
+        function Lsd() {
+          Lsd.__super__.constructor.apply(this, arguments);
+        }
+
+        Lsd.prototype.price = function() {
+          alert("LSD price");
+          return Lsd.__super__.price.call(this, 4);
+        };
+
+        Lsd.prototype.status = function() {
+          alert("LSD status");
+          return Lsd.__super__.status.call(this, "up");
+        };
+
+        return Lsd;
+
+      })(Drug);
+      Crack = (function(_super) {
+
+        __extends(Crack, _super);
+
+        function Crack() {
+          Crack.__super__.constructor.apply(this, arguments);
+        }
+
+        Crack.prototype.price = function() {
+          alert("Crack price");
+          return Crack.__super__.price.call(this, 1);
+        };
+
+        Crack.prototype.status = function() {
+          alert("Crack status");
+          return Crack.__super__.status.call(this, "down");
+        };
+
+        return Crack;
+
+      })(Drug);
+      coke = new Cocaine("Cocaine");
+      lsd = new Lsd("LSD");
+      return crack = new Crack("Crack");
+    });
+    ko.applyBindings(new drugViewModel());
     now.receiveMessage = function(name, message) {
       return $("#messages").append("<br>" + name + ": " + message);
     };
@@ -19,11 +98,9 @@
       now.distributeMessage($("#text-input").val());
       return $("#text-input").val("");
     });
-    $(".change").click(function() {
+    return $(".change").click(function() {
       return now.changeRoom($(this).text());
     });
-    now.name = prompt("What's your name?", "");
-    return ko.applyBindings(new drugViewModel());
   });
 
 }).call(this);
